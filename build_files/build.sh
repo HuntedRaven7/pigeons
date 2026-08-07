435a5574da64ebb8fe17313e59240dd3bb392ce4
Edward Groves <EdwardGroves2007@proton.me>
2026-08-07 00:58:49 -0400

    build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,6 +4,8 @@ set -ouex pipefail
 
 cp -avf "/ctx/system_files"/. /
 
+dnf -y install dnf5-plugins 
+
 dnf -y copr enable lionheartp/Hyprland
 dnf -y install hyprland
 dnf -y copr disable lionheartp/Hyprland
