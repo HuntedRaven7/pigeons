ecfe60906debc9b5a64748d055d523e0869b2608
Edward Groves <EdwardGroves2007@proton.me>
2026-08-07 01:26:05 -0400

    h

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,14 +4,14 @@ set -ouex pipefail
 
 cp -avf "/ctx/system_files"/. /
 
-dnf -y install dnf5-plugins 
+dnf -y install dnf5-plugins gnome* 
 
-dnf -y copr enable --hub copr.fedorainfracloud.org lionheartp/Hyprland fedora-rawhide-x86_64
-dnf repolist --enabled
-dnf repoquery cairo
-dnf repoquery mesa-libEGL
-dnf repoquery xorg-x11-server-Xwayland
-dnf -y install hyprland
-dnf -y copr disable lionheartp/Hyprland
+# dnf -y copr enable --hub copr.fedorainfracloud.org lionheartp/Hyprland fedora-rawhide-x86_64
+# dnf repolist --enabled
+# dnf repoquery cairo
+# dnf repoquery mesa-libEGL
+# dnf repoquery xorg-x11-server-Xwayland
+# dnf -y install hyprland
+# dnf -y copr disable lionheartp/Hyprland
 
 systemctl enable podman.socket
