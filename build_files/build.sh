dabe93953c81e3604ca874c9fc65f06ef4057d59
Edward Groves <EdwardGroves2007@proton.me>
2026-08-07 00:51:58 -0400

    Hummingbird

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,8 +4,8 @@ set -ouex pipefail
 
 cp -avf "/ctx/system_files"/. /
 
-dnf5 -y copr enable lionheartp/Hyprland
-dnf5 -y install hyprland
-dnf5 -y copr disable lionheartp/Hyprland
+dnf -y copr enable lionheartp/Hyprland
+dnf -y install hyprland
+dnf -y copr disable lionheartp/Hyprland
 
 systemctl enable podman.socket
